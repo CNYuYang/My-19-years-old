ad4a759d1403e8d7f04d02df18cd633f202bc9c2
余阳 <744727849@qq.com>
2021-08-08 15:30:54 +0800

    [update]更新联系方式

---
+++ delete-temp-files.sh
@@ -0,0 +1,4 @@
+rm -rf *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex*
+
+
+rm -rf *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
